4de1b484784bfd459f7263eea4f2879b089db6fb
Kamila <kamila.turdugulova1@gmail.com>
2026-06-06 13:20:54 +0600

    скорректировала список CATEGORIES

---
+++ src/constants/categories.ts
@@ -0,0 +1,9 @@
+import type {Category} from "../types";
+
+export const CATEGORIES: Category[] = [
+    { title: 'Фрукты и Овощи', id: 'fruits-veg' },
+    { title: 'Молочные продукты', id: 'dairy' },
+    { title: 'Мясо', id: 'meat' },
+    { title: 'Выпечка и Сладости', id: 'bakery' },
+    { title: 'Напитки', id: 'beverages' }
+];
